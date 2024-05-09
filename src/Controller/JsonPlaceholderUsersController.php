d3957738d93285f52a6c3aaa99d8398549d90946
Patryk Szlachta <patryk.szla@interia.pl>
2024-05-09 13:50:51 +0200

    commit before change controllers names

--- src/Controller/JsonPlaceholderUsersController.php
+++ src/Controller/JsonPlaceholderUsersController.php
@@ -6,18 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Helper\FetchJsonPlaceholderHelper;
+use App\Entity\JsonPlaceholderUser;
 
 class JsonPlaceholderUsersController extends AbstractController
 {
     public function __construct(
         private FetchJsonPlaceholderHelper $jsonHelper,
+        private JsonPlaceholderUser $user
     )
     {
         
     }
 
+    
     #[Route('/fetch-users', name: 'app_json_placeholder_users')]
-    public function index(): Response
+    public function index(JsonPlaceholderUser $jsonPlaceholderUser): Response
     {
         $response = $this->jsonHelper->fetchUsers();
         return $this->render('json_placeholder_users/index.html.twig', [
